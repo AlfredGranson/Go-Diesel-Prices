add14d0d32e60f627b4062ccfb526f9cb8aa93ca
Alfred Granson <alfred.granson@movehq.com>
2022-03-18 11:06:29 -0400

    Build the request URL  On branch main  Changes to be committed: 	modified:   .env.example 	modified:   fetcher.go

--- fetcher.go
+++ fetcher.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "log"
+    "os"
+
+    "github.com/joho/godotenv"
+)
+
+func init() {
+
+    err := godotenv.Load(".env")
+
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+}
 
 func main() {
-	fmt.Println("Hello, world.")
+	var url = os.Getenv("EIA_URL") + "?api_key=" + os.Getenv("EIA_KEY") + "&series_id=" + os.Getenv("EIA_SERIES")
+
+	fmt.Printf(url)
 }
